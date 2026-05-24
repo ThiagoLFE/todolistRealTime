194a50dcc725c4ec094e0025384ae0abb8800f14
ThiagoLFE <thiago.rib1905@gmail.com>
2026-05-24 20:03:47 -0300

    feat: todolist with data star and templ

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	log "log/slog"
 	"net/http"
+	"realtime-todolist/internal/todo"
 	"realtime-todolist/internal/web"
 )
 
 func main() {
 
-	// todolist := todo.InitTodolist()
+	todolist := todo.InitTodolist()
 
 	log.Info("server started on http://localhost:8080")
-	http.ListenAndServe(":8080", web.NewRouter())
+	http.ListenAndServe(":8080", web.NewRouter(&todolist))
 }
